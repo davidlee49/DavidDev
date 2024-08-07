6561e7d6d10e0f4678cb2b075f50e838f163de2e
Ryu Li <ryuli@Ryus-MacBook-Pro.local>
2024-08-06 21:20:41 -0700

    adding selection options

--- app/components/imageCarousel/imageCarouselV2.jsx
+++ app/components/imageCarousel/imageCarouselV2.jsx
@@ -2,13 +2,20 @@
 import { useEffect, useState } from 'react';
 import {ImageList} from '../demoCards'
 
+const url = 'https://images.unsplash.com/photo-1518495973542-4542c06a5843?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D'
+const GameList = [
+    {title: "League of Legends", bgURL: url},
+    {title: "Valorant", bgURL: url},
+    {title: "Call of Dury", bgURL: url}
+]
+
 
 function ImageCarousel({children}) {
   const [backgroundIndex, setBackgroundIndex] = useState(0)
-  const backgrounds = ImageList()
+  const backgrounds = GameList
 
   const getNextBg = (value) => {
-    console.log(backgroundIndex)
+    console.log(backgroundIndex, backgrounds[backgroundIndex])
     if (backgroundIndex + value === backgrounds.length) {
         setBackgroundIndex(0)
     } else if (backgroundIndex + value < 0) {
@@ -20,7 +27,7 @@ function ImageCarousel({children}) {
     <>
         <div style={{position:'absolute' , zIndex:0, top:0, width:'100vw', height:'100vh'}}>
             <img 
-                src={backgrounds[backgroundIndex]}
+                src={backgrounds[backgroundIndex].bgURL}
                 style={{
                     objectFit: 'cover',
                     width: '100%',
@@ -32,8 +39,15 @@ function ImageCarousel({children}) {
         <div style={{position:'absolute' , zIndex:10, top:'50%', padding:'1rem', cursor:'pointer' }} onClick={()=>getNextBg(-1)}> previous </div>
         <div style={{position:'absolute' , zIndex:10, top:'50%', right:0, padding:'1rem', cursor:'pointer' }} onClick={()=>getNextBg(1)}> next </div>
 
-        <div style={{position:'absolute' , zIndex:1, top:0, width:'100%', height:'100%', }}>
-            {children}
+        <div style={{position:'absolute' , zIndex:1, top:0, width:'100%', height:'100%', display:'block'}}>
+            <div style={{display:'flex', justifyContent: 'space-between'}}> {backgrounds[backgroundIndex].title}
+            <div> call button </div>
+            </div>
+
+
+            <div style={{position:'relative'}}>
+            {children}  
+            </div>
         </div>
     </>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,9 @@ export default function Home() {
   return (
       <div>        
         <ImageCarousel>
+          
           <div style={{width:'100%', height:'100%'}}>
-            
+
           </div>
         </ImageCarousel>
 
